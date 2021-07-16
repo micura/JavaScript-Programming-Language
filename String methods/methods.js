46468f455ba9e805366e16104467b345643ae987
micura <micura.adam@gmail.com>
2021-07-16 23:18:01 +0200

    First commit with some stuff

---
+++ String methods/methods.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  //const exampleString = 'Adam GitHub page'
+  let inputString = document.getElementById("inputString").value;
+
+  console.log(inputString)
+  console.log(inputString.length)
+}
