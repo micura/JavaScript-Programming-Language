250af809457223b077af3891b82f9a63a992b3ea
micura <micura.adam@gmail.com>
2021-08-13 23:54:30 +0200

    Repaired div places

--- Move the div/moveTheDiv.js
+++ Move the div/moveTheDiv.js
@@ -4,16 +4,21 @@ const theDivCurrentHeight = document.getElementsByClassName("theDiv")[0].offsetW
 
 function move(pPagePlace) {
 	theDiv.style.position = "absolute";
-	console.log(theDivCurrentSize);
 
-	if (pPagePlace == 'left')
+	if (pPagePlace == 'left') {
 		theDiv.style.top = "50%";
+		theDiv.style.left = "0%";
+	}
 
-	if (pPagePlace == 'right')
-		theDiv.style.top = "100%";
+	if (pPagePlace == 'right') {
+		theDiv.style.top = "50%";
+		theDiv.style.left = "100%";
+	}
 
-	if (pPagePlace == 'top')
+	if (pPagePlace == 'top') {
+		theDiv.style.top = "0%";
 		theDiv.style.left = "50%";
+	}
 
   	if (pPagePlace == 'bottom') {
   		theDiv.style.top = "100%";
